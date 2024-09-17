acf7b1ed677c977016d6a4fda768a4a18cc4f5d4
Ahmed <mohameda@student.aast.edu>
2024-09-17 18:57:10 +0300

    update

--- pages/demo.js
+++ pages/demo.js
@@ -3,41 +3,75 @@ import Layout from "@/components/layout/Layout"
 import Link from "next/link"
 import { useState, useRef, useEffect } from 'react'
 import Waveform from "@/components/elements/Waveform"
-
-
+import RangeSlider from 'react-range-slider-input';
+import 'react-range-slider-input/dist/style.css';
+import Switch from "react-switch";
+import { useRouter } from 'next/navigation';
 
 const audioFiles = [
     {
+        id:1,
         url: '/assets/audio/sampleaudio.mp3',
         waveColor: '#FFFFFF',
         progressColor: 'red',
         size: { height: 50, barHeight: 20, barRadius: 2, barWidth: 3 },
         filename: 'audio1.mp3',
+        isReal:false
     },
-    {
+    {  
+        id:2,
         url: '/assets/audio/sampleaudio.mp3',
         waveColor: '#FFFFFF',
         progressColor: 'green',
         size: { height: 50, barHeight: 20, barRadius: 2, barWidth: 3 },
         filename: 'audio2.mp3',
+        isReal:true
     },
     {
+        id:3,
         url: '/assets/audio/sampleaudio.mp3',
         waveColor: '#FFFFFF',
         progressColor: 'red',
         size: { height: 50, barHeight: 20, barRadius: 2, barWidth: 2 },
         filename: 'audio3.mp3',
+        isReal:false
     },
 ];
 
 export default function Job() {
+    const router = useRouter();
     const [activeIndex, setActiveIndex] = useState(1)
     const [isImageVisible, setImageVisible] = useState(true)
     const [files, setFiles] = useState(audioFiles);
+    const [IsChecked, setIsChecked] = useState(true)
+    const [currentPlaying, setCurrentPlaying] = useState(null);
+
+    const [tokan, setToken] = useState("")
+    useEffect(() => {
+        const tok=localStorage.getItem("token");
+        console.log(tok);
+        if(tok==undefined || tok==""){
+          router.push("/login");
+        }
+      }, []);
 
     const handleOnClick = (index) => {
         setActiveIndex(index)
     }
+
+    const handleChange=(checked) =>{
+        setIsChecked(!checked);
+      }
+
+     
+
+      const handlePlay = (id) => {
+        if (currentPlaying && currentPlaying !== id) {
+          document.querySelector(`button[data-id="${currentPlaying}"]`).click();
+        }
+        setCurrentPlaying(id);
+      };
+
     return (
         <>
             <Layout headerStyle={1} footerStyle={1} breadcrumbTitle={<>Try a <span>Demo</span></>}>
@@ -54,15 +88,44 @@ export default function Job() {
                                         <div className="icon">
                                             <img id="uploadicon" src="assets/img/voice/upload.png" alt="" />
                                         </div>
-                                        <div className="content pb-20">
+                                        <div className="content pb-40">
                                             <a >Upload File</a>
-                                            <p>The best way to get answer faster.</p>
+                                          
                                         </div>
+                                        <div className="content pb-40">
+                                            <p>Frame Length 2</p>
+                                        <RangeSlider
+                                             className="single-thumb"
+                                             defaultValue={[0, 50]}
+                                             thumbsDisabled={[true, false]}
+                                             rangeSlideDisabled={true}
+                                           />
+                                        </div>
+
+                                        <div className="content pb-40">
+                                        <p>Sensitivity 50%</p>
+                                        <RangeSlider
+                                             className="single-thumb"
+                                             defaultValue={[0, 50]}
+                                             thumbsDisabled={[true, false]}
+                                             rangeSlideDisabled={true}
+                                           />
+                                        </div>
+
+                                        <div className="content pb-40">
+                                        <p>Isolate Voice</p>
+                                        <label>
+      
+        <Switch onChange={() => handleChange(IsChecked)} checked={IsChecked} />
+      </label>
+                                        </div>
+
                                         <div className="content">
                                             <div className="pricing-btn">
                                                 <Link href="/login" className="btn btn-two">Upload</Link>
                                             </div>
                                         </div>
+                                    
                                     </div>
 
                                 </div>
@@ -71,13 +134,17 @@ export default function Job() {
                                         <div className="job-item-wrap">
                                             {files.map((audio, index) => (
                                                 <div className="job-item" key={index}>
-                                                    <p>{audio.filename}</p>
+                                                 
                                                     <Waveform
+                                                        key={audio.id}
                                                         audioUrl={audio.url}
                                                         waveColor={audio.waveColor}
                                                         progressColor={audio.progressColor}
                                                         size={audio.size}
                                                         filename={audio.filename}
+                                                        IsReal={audio.isReal}
+                                                        onPlay={() => handlePlay(audio.id)}
+                                                        audioId={audio.id}
                                                     />
                                                 </div>
                                             ))}
@@ -89,52 +156,7 @@ export default function Job() {
                         </div>
                     </section>
 
-                    <section className="counter-area-three pb-130">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-lg-7">
-                                    <div className="counter-content">
-                                        <div className="section-title-four mb-40">
-                                            <h2 className="title">AI can write content just like humans can</h2>
-                                        </div>
-                                        <p>By using Natural Language Processing (NLP) techniques, AI can understand the context, tone, and intent of a given piece of content, and produce written output that's relevant and engaging. This technology is especially useful for generating large volumes of content quickly and accurately, which can save businesses a significant amount of time and resources.</p>
-                                    </div>
-                                </div>
-                                <div className="col-lg-5">
-                                    <div className="counter-item-wrap-three">
-                                        <ul className="list-wrap">
-                                            <li>
-                                                <div className="counter-item-three">
-                                                    <div className="icon">
-                                                        {isImageVisible && (
-                                                            <img id="uploadicon" src="assets/img/voice/playicon.png" onClick={() => handlePlay()} />
-                                                        )}
-                                                        {!isImageVisible && (
-                                                            <img id="uploadicon" src="assets/img/voice/pauseicon.png" onClick={() => handlePause()} />
-                                                        )}
-
-                                                    </div>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <div className="counter-item-three">
-                                                    <div className="icon">
-                                                        <img id="uploadicon" src="assets/img/voice/download.png" alt="" />
-                                                    </div>
-                                                </div>
-                                            </li>
-                                            <li className="threeelist">
-                                                <div className="counter-item-three wavediv">
-                                                </div>
-                                            </li>
-
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-
+                  
                     {/* help-area-end */}
                 </div>
             </Layout>
